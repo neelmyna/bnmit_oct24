2dbdc66e80f4994116aefe2ee86871caf94a3942
neelmyna <nithincheeta@gmail.com>
2024-10-23 09:43:32 +0530

    day1 and day2 material added

---
+++ week1/program1.py
@@ -0,0 +1,8 @@
+num1 = 5
+num2 = 1
+num3 = 4
+num4 = 2
+
+result = num1 + num4 ** num3 ** num2 - 10
+print(result)
+
---
+++ week1/program2.py
@@ -0,0 +1,12 @@
+# Program to check if a number if Even or Odd.
+
+print('Enter a number to check if it is Even: ')
+input_num = int(input())
+
+if input_num % 2 == 0:
+#if x%2==0:
+    print('Input number', input_num, ' is Even')
+else:
+    print('Input number', input_num, ' is Odd')
+
+#In Java, inputNum
