3d77d49af8c8f6f237367a7da07088eff2d84ea4
neelmyna <nithincheeta@gmail.com>
2024-11-14 20:08:24 +0530

    week3 day4 notes added

--- week3/numpy2.py
+++ week3/numpy2.py
@@ -1,46 +1,65 @@
-'''
-Read N (Number of gold coins)
-List of N intergers (specifying value of N Gold coins)
-Read Q (The Number instructions each of which is either HARRY or REMOVE
-Read X (The value at which the Monk will go to sleep)
+import sys
 
-Monk goes to sleep: The sum of gold coins in Monk's bag (Stack)
-Print X as O/P
-'''
+class Node:
+    def __init__(self):
+        self.left  = None
+        self.data  = 0
+        self.right = None
 
-n = int(input('Enter number of Gold coins: '))
-coin_values = []
+class BST:
+    def __init__(self):
+        self.root = None
+        print('An empty BST is created')
 
-print(f'Enter values of {n} Gold coins')
-for i in range(n):
-    coin_values.append(int(input()))
+    def add_node(self, node_data):
+        new_node = Node() # created a new node
+        new_node.data = node_data
 
-q = int(input('Enter number of instructions: '))
-instructions = list()
+        if self.root is None:
+            self.root = new_node
+            return
+        
+        temp1 = self.root
+        temp2 = None
 
-print(f'Enter the {q} instructions')
-for i in range(q):
-    instructions.append(input().strip().lower())
+        while temp1 != None:
+            temp2 = temp1
+            if new_node.data < temp1.data:
+                temp1 = temp1.left
+            else:
+                temp1 = temp1.right
+        if new_node.data < temp2.data:
+            temp2.left = new_node
+        else:
+            temp2.right = new_node
 
-x = int(input('Enter value of X: '))
+    def get_height(self, root):
+        if root == None:
+            return 0 # When tree is empty
+        left_height = self.get_height(root.left)
+        right_height = self.get_height(root.right)
+        return max(left_height, right_height) + 1
+    
+    def inorder(self, root):
+        if root == None:
+            return
+        self.inorder(root.left)
+        print(root.data, end='  ')
+        self.inorder(root.right)
 
-monk_stack = []
-condition_met = False
+def create_tree(bst):
+    number_of_nodes = int(input('Enter number of nodes: '))
+    node_values = []
+    print(f'Enter {number_of_nodes} values of the nodes')
+    for i in range(number_of_nodes):
+        node_values.append(int(input()))
+        bst.add_node(node_values[-1])
 
-print(coin_values, '\n', instructions)
+def run_app(bst):
+    create_tree(bst)
+    print('The BST is')
+    bst.inorder(bst.root)
+    print('\nHeight of the BST is', bst.get_height(bst.root))
 
-j = 0
-for i in range(0, q):
-    if instructions[i] == 'harry' and i < len(coin_values):
-        monk_stack.append(coin_values[j])
-        j += 1
-    elif instructions[i] == 'remove':
-        monk_stack.pop()
-    if sum(monk_stack) == x:
-        condition_met = True
-        break #break the loop
-
-if condition_met:
-    print(f'Number of coins = {len(monk_stack)}')
-else:
-    print('-1')
+bst = BST()
+run_app(bst)
